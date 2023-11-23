21c49710a04d1673118e88851dc6ddd222ac287f
nhutdanh175 <nhutdanh175@gmail.com>
2023-11-23 22:20:17 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 // npm install @apollo/server express graphql cors
 import { ApolloServer } from "@apollo/server";
-import { User } from "@prisma/client";
 import * as dotenv from "dotenv";
-import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { typeDefs } from "./graphql/typeDefs";
 import { resolvers } from "./graphql/resolvers";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { authenticate } from "./auth/auth";
 
 dotenv.config();
 
-export interface Context {
-  currentUser: User | null;
-  decodedIdToken: DecodedIdToken | null;
-}
-
-const server = new ApolloServer<Context>({
+const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
 });
@@ -23,7 +15,4 @@ const server = new ApolloServer<Context>({
 const serverPort = Number(process.env.PORT || 4000);
 startStandaloneServer(server, {
   listen: { port: serverPort },
-  context: async ({ req }) => ({
-    ...(await authenticate(req.headers.authorization)),
-  }),
 }).then(() => console.log(`API Server ready at port ${serverPort}`));
