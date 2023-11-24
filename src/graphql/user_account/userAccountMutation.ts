14ddaf7ff6afd408d77594a9b6d6b2318010e93d
nhutdanh175 <nhutdanh175@gmail.com>
2023-11-24 08:45:00 +0700

    add logout

--- src/graphql/user_account/userAccountMutation.ts
+++ src/graphql/user_account/userAccountMutation.ts
@@ -1,3 +1,5 @@
 export const userAccountMutation = `#graphql
       signUp(email: String!, password: String!): UserAccountID
+      login(email: String!, password: String!): UserAccountID
+      logout: Boolean
 `;
--- src/graphql/user_account/userAccountResolver.ts
+++ src/graphql/user_account/userAccountResolver.ts
@@ -1,4 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { FIREBASE_AUTH } from "../../auth/FirebaseConfig";
 import { prismaClient } from "../../lib/db";
 
@@ -54,6 +58,48 @@ const mutations = {
         console.log("createUserAccount errorMessage: ", errorMessage);
         return null;
       });
+  },
+  login: async (
+    _: any,
+    {
+      email,
+      password,
+    }: {
+      email: string;
+      password: string;
+    }
+  ) => {
+    const auth = FIREBASE_AUTH;
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        return user.uid;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("createUserAccount errorCode: ", errorCode);
+        console.log("createUserAccount errorMessage: ", errorMessage);
+        return null;
+      });
+  },
+  logout: async () => {
+    const auth = FIREBASE_AUTH;
+    await signOut(auth)
+      .then(() => {
+        // Signed up
+
+        return true;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("createUserAccount errorCode: ", errorCode);
+        console.log("createUserAccount errorMessage: ", errorMessage);
+        return null;
+      });
+    return false;
   },
 };
 
