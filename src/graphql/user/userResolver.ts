6be7398925c7b1fc63e4da0857ac0bbdf509ca4b
nhutdanh175 <nhutdanh175@gmail.com>
2023-12-16 09:35:05 +0700

    fix image

--- src/graphql/user/userResolver.ts
+++ src/graphql/user/userResolver.ts
@@ -37,13 +37,14 @@ const queries = {
   },
 };
 
-const uploadImageToCloudinary = async (imagePath: string) => {
-  const cloudinary = require("cloudinary").v2;
+import { v2 as cloudinary } from "cloudinary";
 
+const uploadImageToCloudinary = async (imagePath: string) => {
   cloudinary.config({
     cloud_name: "dxz5uumy7",
     api_key: "272626169313357",
     api_secret: "qiUMjEzYF3_fq7j7OqcxYoVTXOk",
+    secure: true,
   });
 
   console.log(cloudinary.config());
@@ -52,7 +53,10 @@ const uploadImageToCloudinary = async (imagePath: string) => {
   const options: UploadApiOptions = {
     use_filename: true,
     unique_filename: false,
-    overwrite: true,
+    overwrite: false,
+    //upload_preset: "./Food_data2",
+    public_id: "newId",
+    //signature: "mySig",
   };
 
   try {
