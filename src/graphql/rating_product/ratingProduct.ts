d7f3f916c977695cb31a8c6b2e47800973247de0
nhutdanh175 <nhutdanh175@gmail.com>
2023-12-27 09:13:34 +0700

    fix rating

--- src/graphql/rating_product/ratingProduct.ts
+++ src/graphql/rating_product/ratingProduct.ts
@@ -11,7 +11,7 @@ export type CreateRatingProductInputType = {
 export default class RatingProduct {
   static query() {
     return `#graphql
-        getAllRatingOfProduct(productId: ID!): RatingProduct
+        getAllRatingOfProduct(productId: ID!): [RatingProduct]
     `;
   }
   static mutation() {
